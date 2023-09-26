ebd68716962e7f2a0976fb3999dd00d8d32d29f5
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-09-26 17:31:53 -0400

    Fix bug with sys.path import

--- envs.py
+++ envs.py
@@ -1,11 +1,11 @@
 from os import environ as os_environ
-from sys.path import append as sys_path_append
+from sys import path
 from os.path import join as os_path_join, dirname as os_path_dirname
 
 # Add submodule path into import paths
 # is there a better way to handle the sub module path append problem?
 PROJECT_FOLDER = os_path_dirname(__file__)
-sys_path_append(os_path_join(PROJECT_FOLDER, 'transformers'))
+path.append(os_path_join(PROJECT_FOLDER, 'transformers'))
 
 # Define the dataset folder and model folder based on environment
 HOME_DATA_FOLDER = '/home/zhanwen/hgn/data'
